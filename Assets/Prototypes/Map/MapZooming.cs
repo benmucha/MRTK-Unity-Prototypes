78b04127c95bd6e47f0276ba40699bc1a043eabf
benmucha <43706062+benmucha@users.noreply.github.com>
2022-02-22 20:47:21 -0500

    Add Map zooming

--- Assets/Prototypes/Map/MapZooming.cs
+++ Assets/Prototypes/Map/MapZooming.cs
@@ -5,16 +5,30 @@ using Microsoft.MixedReality.Toolkit.UI;
 
 public class MapZooming : MonoBehaviour
 {
-    public PinchSlider slider;
+    /// <summary>
+    /// The slider that lets the user control map zoom magnification.
+    /// </summary>
+    public PinchSlider zoomSlider;
+
+    /// <summary>
+    /// Unscaled Map transform
+    /// </summary>
+    public Transform mapRaw;
+
+    private float minScale = 1;
+    private float maxScale = 5;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        slider.OnValueUpdated.AddListener(OnSliderValueUpdate);
+        zoomSlider.OnValueUpdated.AddListener(OnSliderValueUpdate);
     }
 
     private void OnSliderValueUpdate(SliderEventData eventData)
     {
-        Debug.Log("Slider value update: " + slider.SliderValue);
+        Debug.Log("Slider value update: " + zoomSlider.SliderValue);
+        float mapWidthScale = Mathf.Lerp(minScale, maxScale, zoomSlider.SliderValue);
+        mapRaw.localScale = new Vector3(mapWidthScale, mapWidthScale, 1);
     }
 }
